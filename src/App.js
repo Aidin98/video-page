ef473a3c6d10dfa41570654baefb2d760162097b
Aidin98 <78453568+Aidin98@users.noreply.github.com>
2021-02-28 22:13:30 +0100

    Enter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router ,Switch,Route} from 'react-router-dom'
@@ -8,10 +8,24 @@ import Services from './components/pages/Services'
 
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Loading from './Loading';
 function App() {
+  const  [loading,setLoading]=useState(false)
+  useEffect(()=>{
+      const timer = setTimeout(() => {
+        setLoading(true)
+        console.log('uslo je timeout')
+        console.log(loading)
+  }, 6000);
+  return () => clearTimeout(timer);
+  },[])
   return (
     <>
-    <Router>
+    {loading ? (
+      
+    
+<Router>
+    
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home}/>
@@ -20,6 +34,10 @@ function App() {
         <Route path='/sign-up' component={SignUp}/>
       </Switch>
       </Router>
+    ):(
+      <Loading />
+    )}
+    
     </>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loading.css'
+function Loading() {
+    return (
+        <div>
+            <video src='https://player.vimeo.com/external/462033983.sd.mp4?s=0ae597755a70e6c8f51c55b612b543a6a95fdc2c&profile_id=164' autoPlay loop muted />
+        </div>
+    )
+}
+
+export default Loading
