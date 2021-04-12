2aa8ae227a76000bea9a344b55f32b8d473f2b75
Aidin98git config --global user.email aidin.basic@edu.fit.baaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git initReinitialized existing Git repository in C:/Users/aidin/OneDrive/Desktop/web d/memory-server/.git/aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git add .aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git commit -m init[master (root-commit) ed00cbc] init 13 files changed, 3554 insertions(+) create mode 100644 .gitignore create mode 100644 graphql/resolvers/comments.js create mode 100644 graphql/resolvers/index.js create mode 100644 graphql/resolvers/posts.js create mode 100644 graphql/resolvers/users.js create mode 100644 graphql/typeDefs.js create mode 100644 index.js create mode 100644 models/Post.js create mode 100644 models/User.js create mode 100644 package-lock.json create mode 100644 package.json create mode 100644 util/check-auth.js create mode 100644 util/validators.jsaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git remote add origin git@github.com:Aidin98/memory-blog-server.gitaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git remote -vorigin  git@github.com:Aidin98/memory-blog-server.git (fetch)origin  git@github.com:Aidin98/memory-blog-server.git (push)aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git push origin masterThe authenticity of host 'github.com (140.82.121.3)' can't be established.RSA key fingerprint is SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8.Are you sure you want to continue connecting (yes/no/[fingerprint])? yPlease type 'yes', 'no' or the fingerprint: yesWarning: Permanently added 'github.com,140.82.121.3' (RSA) to the list of known hosts.git@github.com: Permission denied (publickey).fatal: Could not read from remote repository.Please make sure you have the correct access rightsand the repository exists.aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git logingit: 'login' is not a git command. See 'git --help'.The most similar command is        columnaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git config --global user.name Aidin98 <78453568+Aidin98@users.noreply.github.com>
2021-04-12 09:37:04 +0200

    Enter

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,63 +9,86 @@ function Cards() {
     Aos.init({duration:3000})
   })
   return (
-    <div  className='cards' style={{background:'lightgrey'}}>
-      <h1 data-aos='fade-right'>About me!</h1>
-      <div className='about'><Avatar data-aos='fade-right'  className='slika' src='images/slika.jpg' style={{height:'250px',width:'250px'}}/>
-      <div data-aos='fade-right' className='information'>
-        <h1>Hi I am Aidin Bašić</h1>
-      <p>I am highly organised, dedicated Junior Web developer with ability to improve working environments and a commintment to employee welfare.I posses strong Web development skills and i am good at solving problemsMy areas of intersts include computer system, software devlopment, and web develoment. I am commited to continious learning and proffesional development within the IT fields. I have produced a couple personal projects that you can see at my GitHub profile</p>
+    <div className="cards" style={{ background: "lightgrey" }}>
+      <h1 data-aos="fade-right">About me!</h1>
+      <div className="about">
+        <Avatar
+          data-aos="fade-right"
+          className="slika"
+          src="images/slika.jpg"
+          style={{ height: "250px", width: "250px" }}
+        />
+        <div data-aos="fade-right" className="information">
+          <h1>Hi I am Aidin Bašić</h1>
+          <p>
+            I am highly organised, dedicated Junior Web developer with ability
+            to improve working environments and a commintment to employee
+            welfare.I posses strong Web development skills and i am good at
+            solving problemsMy areas of intersts include computer system,
+            software devlopment, and web develoment. I am commited to continious
+            learning and proffesional development within the IT fields. I have
+            produced a couple personal projects that you can see at my GitHub
+            profile
+          </p>
+        </div>
       </div>
-      
-      
-      </div>
-      <div className='education'>
-        <div className='school' data-aos='flip-up'>
-          <div style={{display:'flex' ,flexDirection:'column',justifyContent:'space-between' }}>
+      <div className="education">
+        <div className="school" data-aos="flip-up">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
             <h1>Education</h1>
-              <ul className='list'>
-              <li>Univerzitet Dzemal Bijedic --- 10/2017-11/2020</li>
-               I am bachelor od Bussiness Informatics and trought my college i have
-learnd alot about programming languages and It in general.
-
-<li>Spark School --- 02/2020-05/2020 </li>
-I was participating a web development curs where we were learning abou
-web programming languages like HTML and JavaScript.
-
-              </ul>
+            <ul className="list">
+              <li>Univerzitet Dzemal Bijedic --- 10/2017-11/2020</li>I am
+              bachelor od Bussiness Informatics and trought my college i have
+              learnd alot about programming languages and It in general.
+              <li>Spark School --- 02/2020-05/2020 </li>I was participating a
+              web development curs where we were learning abou web programming
+              languages like HTML and JavaScript.
+            </ul>
           </div>
         </div>
-        <div className='work' data-aos='flip-up'>
+        <div className="work" data-aos="flip-up">
           <h1>Work Experience</h1>
           <ul>
             <li>UniCredit Bank Mostar</li>
             • archivist
-            <br/>
-Scanning searching and storage of documents.
-
+            <br />
+            Scanning searching and storage of documents.
           </ul>
         </div>
       </div>
-      <h1 className='project'>Check out some of my projects</h1>
-       <div data-aos='fade-flip' className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items' style={{background:'#fff',padding:'10px',borderRadius:'30px'}}>
-            <CardItem 
-              src='images/facebook.PNG'
-              text='Check out my live Facebook Clone'
-              label='React'
-              path='https://fb-clone-with-firebase-2u7e6h1hk.vercel.app/'
-            />
-            
+      <h1 className="project">Check out some of my projects</h1>
+      <div data-aos="fade-flip" className="cards__container">
+        <div className="cards__wrapper">
+          <ul
+            className="cards__items"
+            style={{
+              background: "#fff",
+              padding: "10px",
+              borderRadius: "30px",
+            }}
+          >
             <CardItem
-              src='images/aaa.PNG'
-              text='Check out my live Netflix page'
-              label='React'
-              path='https://netflix-clone-ten-sage.vercel.app/'
+              src="images/memory.PNG"
+              text="Check out my live Memory Blog"
+              label="MERN Stack"
+              path="https://memory-blog.netlify.app/"
+            />
+
+            <CardItem
+              src="images/aaa.PNG"
+              text="Check out my live Netflix page"
+              label="React"
+              path="https://netflix-clone-ten-sage.vercel.app/"
             />
           </ul>
-          </div>
-    </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -4,53 +4,84 @@ import CardItem from '../CardItem'
 import './Card.css'
 import './Products.js'
 export default function Products (){
-    return(
-      <div className='container' style={{position:'relative',paddingTop:'25px'}}> 
-        <video src='../videos/video-2.mp4' autoPlay loop muted />
-         <div className='cards' style={{background:'transparent',position:'relative'}}>
-          
-      <h1 style={{color:'#fff'}}>Check out these EPIC Projects!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items' style={{background:'#fff',padding:'10px',borderRadius:'30px'}}>
-            <CardItem 
-              src='images/facebook.PNG'
-              text='Check out my live Facebook Clone'
-              label='React'
-              path='https://fb-clone-with-firebase-2u7e6h1hk.vercel.app/'
-            />
-            
-            <CardItem
-              src='images/aaa.PNG'
-              text='Check out my live Netflix page'
-              label='React'
-              path='https://netflix-clone-ten-sage.vercel.app/'
-            />
-          </ul>
-          <ul className='cards__items' style={{background:'#fff',padding:'10px',borderRadius:'30px'}}>
-            <CardItem
-              src='images/message.PNG'
-              text='Check out my Message App'
-              label='React'
-              path='https://message-app-eta.vercel.app/'
-            />
-            <CardItem
-              src='images/ultra.PNG'
-              text='Check out one of my designs'
-              label='React'
-              path='https://ultra-page-styled-components.vercel.app/'
-            />
-            <CardItem
-              src='images/amazon.PNG'
-              text='Check out my Amazone clone'
-              label='React'
-              path='https://amazon-clone-seven.vercel.app/'
-            />
-          </ul>
+    return (
+      <div
+        className="container"
+        style={{ position: "relative", paddingTop: "25px" }}
+      >
+        <video src="../videos/video-2.mp4" autoPlay loop muted />
+        <div
+          className="cards"
+          style={{ background: "transparent", position: "relative" }}
+        >
+          <h1 style={{ color: "#fff" }}>Check out these EPIC Projects!</h1>
+          <div className="cards__container">
+            <div className="cards__wrapper">
+              <ul
+                className="cards__items"
+                style={{
+                  background: "#fff",
+                  padding: "10px",
+                  borderRadius: "30px",
+                }}
+              >
+                <CardItem
+                  src="images/facebook.PNG"
+                  text="Check out my live Memory Blog"
+                  label="MERN Stack"
+                  path="https://memory-blog.netlify.app/"
+                />
+
+                <CardItem
+                  src="images/aaa.PNG"
+                  text="Check out my live Netflix page"
+                  label="React"
+                  path="https://netflix-clone-ten-sage.vercel.app/"
+                />
+              </ul>
+              <ul
+                className="cards__items"
+                style={{
+                  background: "#fff",
+                  padding: "10px",
+                  borderRadius: "30px",
+                }}
+              >
+                <CardItem
+                  src="images/message.PNG"
+                  text="Check out my Message App"
+                  label="React"
+                  path="https://message-app-eta.vercel.app/"
+                />
+                <CardItem
+                  src="images/ultra.PNG"
+                  text="Check out one of my designs"
+                  label="React"
+                  path="https://ultra-page-styled-components.vercel.app/"
+                />
+                <CardItem
+                  src="images/amazon.PNG"
+                  text="Check out my Amazone clone"
+                  label="React"
+                  path="https://amazon-clone-seven.vercel.app/"
+                />
+              </ul>
+            </div>
+          </div>
+          <h1 style={{ color: "#fff", display: "flex" }}>
+            For more projects check out my GitHub page{" "}
+            <a
+              style={{ display: "table-cell" }}
+              href="https://github.com/Aidin98"
+              target="_blank"
+            >
+              <i
+                class="fas fa-arrow-right"
+                style={{ color: "white", marginLeft: "10px" }}
+              ></i>
+            </a>
+          </h1>
         </div>
       </div>
-      <h1 style={{color:'#fff',display:'flex'}}>For more projects check out my GitHub page <a style={{display: "table-cell"}} href='https://github.com/Aidin98' target="_blank"><i class="fas fa-arrow-right" style={{color:'white',marginLeft:'10px'}}></i></a></h1>
-    </div>
-    </div>
-    )
+    );
 }
